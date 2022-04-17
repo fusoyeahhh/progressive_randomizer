348b0e68281df4abe5078f9e83c73eff9bc6c200
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-04-17 10:23:18 -0700

    progressive_randomizer: restructure to hierarchy

--- progressive_randomizer/__main__.py
+++ progressive_randomizer/__main__.py
@@ -1,25 +1,42 @@
+import pprint
 import fire
 
-from . import FF6StaticRandomizer
-from . import WriteQueue
+from . import StaticRandomizer, WriteQueue, SNESHeader
 
 import logging
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
-log.setLevel(logging.INFO)
 
-#rom = open("ff6.smc", "rb").read()
+def _read_header(filename):
+    with open(filename, "rb") as fin:
+        romdata = fin.read(0x10000)
 
-#rando = FF6StaticRandomizer()
-#pprint.pprint(rando._reg._blocks)
-#pprint.pprint({**rando._reg._tags})
+    return SNESHeader() << romdata
+
+def autodetect_and_load_game(filename):
+    from .game import KNOWN_GAMES
+    header_data = _read_header(filename)
+    game_name = header_data["Game Title Registration"]
+
+    if game_name not in KNOWN_GAMES:
+        log.warning("Game does not have a registered randomizer, "
+                    "using default --- some functions may not be available.")
+    rando = KNOWN_GAMES.get(game_name, StaticRandomizer)
+    log.info(f"Read header, game name: {game_name} -> {rando}")
+
+    with open(filename, "rb") as fin:
+        romdata = fin.read()
+
+    return romdata, rando
 
 class DoAThing:
     def __init__(self, filename="ff6.smc"):
-        with open(filename, "rb") as fin:
-            self._romdata = fin.read()
-
-        self._rando = FF6StaticRandomizer()
+        self._filename = filename
+        self._romdata, self._rando = autodetect_and_load_game(filename)
         self._q = WriteQueue()
+
+    def print_header(self):
+        return pprint.pformat(_read_header(self._filename))
 
     def print_tags(self):
         return {**self._rando._reg._tags}
---
+++ progressive_randomizer/game/__init__.py
@@ -0,0 +1,6 @@
+from .ff6 import GAME_NAME as FF6_US_GAME_NAME
+from .ff6 import FF6StaticRandomizer
+
+KNOWN_GAMES = {
+    FF6_US_GAME_NAME: FF6StaticRandomizer
+}
