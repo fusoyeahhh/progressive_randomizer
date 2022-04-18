9633fc53b3da26b988a0366e91c80da3af914293
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-04-17 19:13:45 -0700

    tasks: clean up and interval representation

--- progressive_randomizer/tasks/__init__.py
+++ progressive_randomizer/tasks/__init__.py
@@ -1,8 +1,12 @@
 """
 Generic randomization tasks.
 """
+import random
 
 class RandomizationTask:
+    def __init__(self, memblk):
+        self._memblk = memblk
+
     def __call__(self, bindata):
         return self._memblk << bindata
 
@@ -10,12 +14,18 @@ class RandomizationTask:
         data = self(bindata)
         return self._memblk @ bytes(data) >> bindata
 
-class ShuffleBytes(RandomizationTask):
-    def __init__(self, memblk):
-        self._memblk = memblk
+    # Determines whether two randomizations could collide
+    def __and__(self, rhs):
+        import portion
+        lhs = portion.closedopen(*self.affected_blocks())
+        rhs = portion.closedopen(*rhs.affected_blocks())
+        return lhs.intersection(rhs) != portion.empty()
 
+    def affected_blocks(self):
+        return (self._memblk.addr, self._memblk.addr + self._memblk.length)
+
+class ShuffleBytes(RandomizationTask):
     def __call__(self, bindata):
-        import random
         data = super().__call__(bindata)
         return random.sample(list(data), k=len(data))
 
