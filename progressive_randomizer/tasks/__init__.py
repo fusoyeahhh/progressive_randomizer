25f3214251638dd0257119753cf9efaae4750076
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-05-27 19:22:42 -0700

    tasks: add base stringification

--- progressive_randomizer/tasks/__init__.py
+++ progressive_randomizer/tasks/__init__.py
@@ -7,6 +7,9 @@ class RandomizationTask:
     def __init__(self, memblk):
         self._memblk = memblk
 
+    def __str__(self):
+        return f"{self.__class__.__name__} -> {self._memblk}"
+
     def __call__(self, bindata):
         # FIXME: we have to ensure this binary data, because our read method returns
         # different things
