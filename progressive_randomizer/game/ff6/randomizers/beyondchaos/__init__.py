58c246c1df832e0b8acae8ced17e916551189afc
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-05-27 19:25:57 -0700

    beyondchaos: start beyond chaos rando translation

--- progressive_randomizer/game/ff6/randomizers/beyondchaos/__init__.py
+++ progressive_randomizer/game/ff6/randomizers/beyondchaos/__init__.py
@@ -1,4 +1,16 @@
+import logging
+log = logging.getLogger()
+
 from ... import FF6StaticRandomizer
+from .....tasks import (
+    RandomizationTask,
+    WriteBytes
+)
+
+from . import substitutions
+from .substitutions import SubstitutionTask
+
+from BeyondChaos import utils as bc_utils
 
 GAME_NAME = b'FF6 BCCE'
 
@@ -6,3 +18,50 @@ def detect_bc(game_name):
     if game_name.startswith(GAME_NAME):
         return FF6StaticRandomizer
     return None
+
+class BeyondChaosRandomizer(FF6StaticRandomizer):
+
+    def __init__(self, seed, flags):
+        super().__init__()
+        self._seed = seed
+        self._flags = flags
+
+    """
+    def christmas_mode(self, bindata):
+        pass
+
+    def halloween_mode(self, bindata):
+        pass
+        
+    def random_boost(self, bindata):
+        pass
+
+    def scenario_not_taken(self):
+        pass
+    """
+
+    #
+    # From randomizer.py
+    #
+    # NOTE: these have requirements in Options_
+    def auto_recruit_gau(self, **kwargs):
+        # FIXME: use this
+        stays_in_wor = kwargs.get("stays_in_wor", True)
+        return [
+            SubstitutionTask.sub_with_args(location=0xCFE1A,
+                                           sub=bc_utils.AutoRecruitGauSub),
+            SubstitutionTask.sub_with_args(location=0x24856,
+                                           bytestring=bytes[0x89, 0xFF],
+                                           sub=bc_utils.Substitution),
+        ]
+
+    def auto_learn_rage(self):
+        return [SubstitutionTask.sub_with_args(location=0x23B73,
+                                               sub=bc_utils.AutoLearnRageSub)
+        ]
+
+    def manage_commands(self):
+        return [
+            SubstitutionTask.sub_with_args(**task)
+            for name, task in substitutions.manage_commands_writes.items()
+        ]
---
+++ progressive_randomizer/game/ff6/randomizers/beyondchaos/substitutions.py
@@ -0,0 +1,109 @@
+from .....tasks import (
+    RandomizationTask,
+    WriteBytes
+)
+
+from .....components import (
+    AssemblyObject,
+    MemoryStructure
+)
+
+from BeyondChaos import utils
+
+# FIXME: Do we want to inherit from WriteBytes?
+class SubstitutionTask(WriteBytes):
+    """
+    Wrapper for the Subsitution class writer from BC.
+    """
+    @classmethod
+    def sub_with_args(cls, location=None, bytestring=None,
+                      sub=utils.Substitution, **kwargs):
+        sub = sub(**kwargs)
+        sub.set_location(location)
+        sub.bytestring = bytestring
+        # TODO: callback?
+        return cls(sub)
+
+    @classmethod
+    def from_dict(cls, subs):
+        return [cls.sub_with_args(**args) for name, args in subs.items()]
+
+    def __init__(self, substitution):
+        self._sub = substitution
+        subs = MemoryStructure(addr=self._sub.location,
+                               length=len(self._sub.bytestring),
+                               name=str(self._sub), descr="dummy memblk")
+        super().__init__(subs, self._sub.bytestring)
+
+    def apply(self, bindata):
+        from io import BytesIO
+        buffer_writer = BytesIO(bindata)
+        self._sub.write(buffer_writer)
+        return buffer_writer.getvalue()
+
+    def as_assembly(self):
+        return AssemblyObject(addr=self._sub.location,
+                              length=len(self._sub.bytestring),
+                              name=str(self._sub),
+                              descr="substitution assembly block")
+
+manage_commands_writes = {
+    "learn_lore_sub": {
+        "location": 0x236E4,
+        "bytestring": bytes([0xEA, 0xEA, 0xF4, 0x00, 0x00, 0xF4, 0x00, 0x00])
+    },
+
+    "learn_dance_sub": {
+        "location": 0x25EEB,
+        "bytestring": bytes([0xEA, 0xEA])
+    },
+
+    "learn_swdtech_sub": {
+        "location": 0x261C7,
+        "bytestring": bytes([0xEB, 0x48, 0xEB, 0xEA])
+    },
+    "learn_swdtech_sub_2": {
+        "location": 0x261D3,
+        "bytestring": bytes([0x68, 0xEB] + [0xEA] * 4)
+    },
+
+    "learn_blitz_sub": {
+        "location": 0x261CE,
+        "bytestring": bytes([0xF0, 0x09]),
+    },
+    "learn_blitz_sub_2": {
+        "location": 0x261D3,
+        "bytestring": bytes([0xD0, 0x04])
+    },
+    "learn_blitz_sub_3": {
+        "location": 0x261D9,
+        "bytestring":  bytes([0x68, 0xEB, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA])
+    },
+    "learn_blitz_sub_4": {
+        "location": 0x261E3,
+        "bytestring": bytes([0xEA] * 4),
+    },
+    "learn_blitz_sub_5": {
+        "location": 0xA200,
+        "bytestring": bytes([0xEA]),
+    },
+
+    "learn_multiple_sub": {
+        "location": 0xA1B4,
+        "bytestring": bytes([0xF0, 0xFE - (0xA1B4 - 0xA186)])
+    },
+    "learn_multiple_sub_2": {
+        "location": 0xA1D6,
+        "bytestring": bytes([0xF0, 0xFE - (0xA1D6 - 0xA18A)])
+    },
+    "learn_multiple_sub_3": {
+        "location": 0x261DD,
+        "bytestring": bytes([0xEA] * 3)
+    },
+
+    "range_blank_sub": {
+        "location": 0x47AA0,
+        "bytestring": bytes([0x01] + ([0x00] * 31))
+    },
+}
+
