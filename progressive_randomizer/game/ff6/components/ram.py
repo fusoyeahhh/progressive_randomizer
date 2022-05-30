e2e1a93d753615acca12df676084ec8b80f38019
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-05-29 19:32:25 -0700

    ff6/ram: add helper to get currently pressed buttons

--- progressive_randomizer/game/ff6/components/ram.py
+++ progressive_randomizer/game/ff6/components/ram.py
@@ -1,6 +1,23 @@
 from .... import MemoryLayoutParser
 from ....components import Registry
 
+from enum import IntFlag
+
+class ButtonPressed(IntFlag):
+    NoButton = 0
+    Right = 1 << 0
+    Left = 1 << 1
+    Down = 1 << 2
+    Up = 1 << 3
+    Start = 1 << 4
+    Select = 1 << 5
+    Y = 1 << 6
+    B = 1 << 7
+    R = 1 << 12
+    L = 1 << 13
+    X = 1 << 14
+    A = 1 << 15
+
 class FF6SRAM(Registry):
     def __init__(self):
         super().__init__()
@@ -27,3 +44,11 @@ class FF6BattleRAM(Registry):
 
         for blk in blks:
             self.register_block(**blk)
+
+    @classmethod
+    def parse_header_block(cls, data):
+        return {
+            "buttons_this_frame": ButtonPressed(int.from_bytes(data[0xA:0xC], byteorder="big")),
+            "buttons_last_frame": ButtonPressed(int.from_bytes(data[0xC:0xE], byteorder="big")),
+            "frame_counter": data[0xE]
+        }
