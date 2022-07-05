8213c2875c2674b5dadf6fc60293f8015d5c3650
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-07-05 00:36:56 -0700

    managers.py: add item manager / rando

--- progressive_randomizer/game/ff6/managers/__init__.py
+++ progressive_randomizer/game/ff6/managers/__init__.py
@@ -1,14 +1,33 @@
 import random
+from dataclasses import dataclass, asdict
 import logging
 log = logging.getLogger()
+
+#from .. import data
 
 from ....components import MemoryStructure
 from ....utils.randomization import random_prob, choice_without_replacement
 from ....tasks import WriteBytes
 
-from ..components import FF6Text
-from ..randomizers import FF6StaticRandomizer
-from ..data import Character, Command
+from ..components import (
+    FF6Text,
+    FF6SRAM,
+    FF6ItemTable
+)
+from ..randomizers import (
+    FF6StaticRandomizer,
+    FF6ProgressiveRandomizer,
+    AttributeRandomizer,
+    SpellRandomizer,
+    StatusRandomizer,
+    StatRandomizer
+)
+from ..data import (
+    Character,
+    Command,
+    Item,
+    InventoryType
+)
 
 class FF6CommandManager(FF6StaticRandomizer):
     def __init__(self):
@@ -155,4 +174,136 @@ class FF6CharacterManager(FF6StaticRandomizer):
         for c in self.chr_data.values():
             if Command.Morph in c.commands:
                 return [WriteBytes(self._MORPH_MEMBLK, bytes([0xC9, c.idx]))]
-        return []
+        return []# Thought: MI from StaticRandomizer and FF6ItemTable?
+class ItemManager(FF6StaticRandomizer):
+    _BASE_ITEM_TMPLT = FF6ItemTable.ItemEntry
+
+    def handle_spell_proc(self, item, spell=None, random_cast=None, inv_remove=None):
+        item["cast_spell"] = spell or AttributeRandomizer.spells(skillsets={"magic"})
+        item["random_cast"] = random_cast or bool(random.randint(0, 1))
+        item["inv_remove"] = inv_remove or bool(random.randint(0, 1))
+        return item
+
+
+    def handle_spell_learning(self, item, spell=None, closeness=50):
+        # TODO: handle tiering
+        item["learned_spell"] = spell or AttributeRandomizer.spells(skillsets={"magic"})
+        lr = max(1, item["learn_rate"])
+        item["learn_rate"] = AttributeRandomizer.spells.gen_learning_rate(lr, closeness)
+        return item
+
+    def generate(self, base, tmplt=None, mixing_ratio=0.1, closeness=100, keep=set(),
+                 element_gen=0.1, status_gen=0.1, field_effect_gen=0.1, equip_effect_gen=0.1,
+                 learn_spell_prob=0.01, gen_proc_prob=0.05):
+        assert tmplt is None or base.item_type == tmplt.item_type
+
+        base_item = asdict(base)
+        new_item = base_item.copy()
+        for attr in self._BASE_ITEM_TMPLT.__dataclass_fields__:
+            if attr in keep:
+                log.info(f"base {base.name} | attr {attr}: {base_item[attr]} -> NO CHANGE")
+                continue
+
+            if tmplt is not None and random.uniform(0, 1) < mixing_ratio:
+                new_item[attr] = tmplt[attr]
+
+            # FIXME: for both spells, we can modify rates / flags independently
+            # if they are valid
+            if attr == "learned_spell" \
+                and (new_item["learn_rate"] > 0 or learn_spell_prob < random.uniform(0, 1)):
+                self.handle_spell_learning(new_item, closeness=closeness)
+            elif attr == "cast_spell" \
+                 and (new_item["random_cast"] > 0 or new_item["inv_remove"]
+                   or learn_spell_prob < random.uniform(0, 1)):
+                self.handle_spell_learning(new_item, closeness=closeness)
+            elif attr == "equipped_by":
+                new_item[attr] = AttributeRandomizer.equipchar.shuffle(new_item[attr],
+                                                                       mix_ratio=0.5,
+                                                                       fuzzy=True)
+            elif attr == "field_effect":
+                new_item[attr] = AttributeRandomizer.fieldeffect.shuffle(new_item[attr],
+                                                                         fuzzy=True,
+                                                                         generate=field_effect_gen)
+            elif attr == "equip_flags":
+                new_item[attr] = AttributeRandomizer.equipflags.shuffle(new_item[attr],
+                                                                        fuzzy=True,
+                                                                        generate=equip_effect_gen)
+            elif attr == "status_1":
+                new_item[attr] = AttributeRandomizer.status.shuffle(new_item[attr],
+                                                                    fuzzy=True,
+                                                                    generate=status_gen,
+                                                                    only_bytes={0})
+            elif attr in "status_2":
+                new_item[attr] = AttributeRandomizer.status.shuffle(new_item[attr],
+                                                                    fuzzy=True,
+                                                                    generate=status_gen,
+                                                                    only_bytes={1})
+            elif attr in "equip_status":
+                new_item[attr] = AttributeRandomizer.status.shuffle(new_item[attr],
+                                                                    fuzzy=True,
+                                                                    generate=status_gen,
+                                                                    only_bytes={2})
+            elif attr in "_equipment_status":
+                new_item[attr] = AttributeRandomizer.status.shuffle(new_item[attr],
+                                                                    fuzzy=True,
+                                                                    generate=status_gen,
+                                                                    only_bytes={1})
+            elif attr == "elemental_data":
+                new_item[attr] = AttributeRandomizer.element.shuffle(new_item[attr],
+                                                                     fuzzy=True,
+                                                                     generate=element_gen)
+            elif attr == "targeting":
+                new_item[attr] = AttributeRandomizer.spelltargeting(new_item[attr])
+            elif attr in {"vigor", "speed", "stamina", "magic"}:
+                new_item[attr] = StatRandomizer(-8, 8)(new_item[attr], closeness)
+            elif attr in {"evade", "magic_evade"}:
+                new_item[attr] = StatRandomizer(0, 16)(new_item[attr], closeness)
+            elif attr == "power":
+                # This wpn pwr / armor def / item heal
+                new_item[attr] = StatRandomizer(0, 255)(new_item[attr], closeness)
+            elif attr == "actor_status_1":
+                # This wpn hit rate / armor mag def / item status
+                if base.item_type == InventoryType.Item:
+                    new_item[attr] = AttributeRandomizer.status.shuffle(new_item[attr],
+                                                                        fuzzy=True,
+                                                                        only_bytes={0})
+                else:
+                    new_item[attr] = StatRandomizer(0, 255)(new_item[attr], closeness)
+            elif attr in {"actor_status_2", "actor_status_3", "actor_status_4"}:
+                # This wpn and armor elem {absorb,null,weak} / item status
+                if base.item_type == InventoryType.Item:
+                    new_item[attr] = AttributeRandomizer.status.shuffle(new_item[attr],
+                                                                        fuzzy=True,
+                                                                        only_bytes={0})
+                else:
+                    new_item[attr] = AttributeRandomizer.element.shuffle(new_item[attr],
+                                                                         fuzzy=True,
+                                                                         generate=element_gen)
+            elif attr == "price":
+                new_item[attr] = StatRandomizer(0, 2**16 - 1)(new_item[attr], closeness)
+
+            log.info(f"base {base.name} | {attr}: {base_item[attr]} -> {new_item[attr]}")
+
+        return self._BASE_ITEM_TMPLT(**new_item)
+
+    def randomize_items(self, bindata):
+        item_names = FF6Text._decode(self["itm_nms"] << bindata, 13)
+        item_data = dict(zip(item_names, self["itm_dt"] << bindata))
+
+        # FIXME: maybe the item object should know where to retrieve its own name / data?
+        for name, key in zip(item_names, item_data):
+            item_data[name].name = name
+
+        log.info(f"Decoded {len(item_data)} items")
+
+        for name in item_names:
+            # Don't randomize Empty, as it has invalid values
+            if name == " Empty".ljust(13, " "):
+                continue
+            #name = "_Blizzard".ljust(13, " ")
+            #print(name, item_data[name])
+            print(name)
+            print(self.generate(item_data[name], mixing_ratio=1.0))
+
+    def write_spoiler(self):
+        pass
