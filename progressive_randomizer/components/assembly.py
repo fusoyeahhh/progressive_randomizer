e16bb06fa81078a200b5294f7fe5e9b2f5994fb9
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-07-09 21:20:04 -0700

    assembly.py: begin work on a assembler

--- progressive_randomizer/components/assembly.py
+++ progressive_randomizer/components/assembly.py
@@ -1,5 +1,6 @@
 import csv
 import functools
+from dataclasses import dataclass
 
 import logging
 log = logging.getLogger()
@@ -78,11 +79,86 @@ class AssemblyObject(MemoryStructure):
     def annotate(self, bindata):
         return self._annotate(self.read(bindata))
 
-    FLOW_OPS = {"JSR", "JSL", "RTS", "RTL", "JMP", "JML",
-                "BEQ", "BNE", "BMI", "BPL", "BCS", "BCC", "BVS", "BVC", "BRA", "BRL"}
+class Assembly:
 
-    def identify_pointers(self, bindata):
+    @dataclass
+    class Instruction:
+        ext_name: str
+        descr: str
+        opcode: int
+        mode: str
+        flags: str
+        args: int
+
+        FLOW_OPS = {"JSR", "JSL", "RTS", "RTL", "JMP", "JML",
+                    "BEQ", "BNE", "BMI", "BPL", "BCS", "BCC",
+                    "BVS", "BVC", "BRA", "BRL"}
+
+        OP_REF = {}
+        with open("etc/snes_op_code_ref.csv", "r") as fin:
+            OP_REF = {int(item[3], base=16): {
+                    "ext_name": item[0],
+                    "descr": item[1],
+                    "opcode": int(item[3], base=16),
+                    "mode": item[4],
+                    "flags": item[5],
+                    "args": int(item[6][0]),
+                }
+                for item in csv.reader(fin.readlines())
+            }
+        _BY_NAME = {instr.ext_name: instr for instr in OP_REF.values()}
+
+        @classmethod
+        def from_int(cls, opcode):
+            return cls(**cls.OP_REF[opcode])
+
+        @classmethod
+        def from_text(cls, text):
+            return cls(**cls._BY_NAME[text])
+
+        @property
+        def name(self):
+            return self.ext_name.split(" ")[0]
+
+        def _validate_args(self, mode, *args):
+            pass
+
+        def __int__(self):
+            return self.opcode
+
+        def __call__(self, *args):
+            # TODO: validate args against mode
+            assert len(args) == self.args
+            formatted_args = " ".join([f"0x{arg:02x}" for arg in args])
+            return f"{self.ext_name} {formatted_args}"
+
+        def _(self, *args):
+            # TODO: validate args against mode
+            return bytes([self.opcode] + list(args))
+
+    @classmethod
+    def from_text(cls, text, comment="#"):
+        assembly_code = []
+        byte_code = b''
+        for line in text.split("\n"):
+            line = line.split(comment)[0].strip()
+            if line == "":
+                continue
+            cmd, args = line[0], [int(a, base=16) for a in line[1:]]
+            assembly_code.append((cmd, args))
+            byte_code += b"".join([int(cmd)] + args)
+
+        return assembly_code, byte_code
+
+    def __init__(self, text=""):
+        self._instr = []
+        self._byte_code = b""
+
+        self._instr, self._byte_code = self.from_text(text)
+
+    def identify_pointers(self):
         pass
+
 
 class AssemblyEditor(AssemblyObject):
     @classmethod
