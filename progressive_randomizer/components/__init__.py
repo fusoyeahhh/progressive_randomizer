fb60165dd6fbd46ab3fcd93ce6b47fb7bb692bdb
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-07-10 11:58:58 -0700

    components: documentation

--- progressive_randomizer/components/__init__.py
+++ progressive_randomizer/components/__init__.py
@@ -11,6 +11,10 @@ log = logging.getLogger()
 
 @dataclass(repr=True, init=True)
 class MemoryStructure:
+    """
+    The MemoryStructure object is the root of most of the component hierarchy.
+    It is a generic container describing a contiguous stretch of byte data, having some metadata associated with it for tracking.
+    """
     addr: int
     length: int
     name: str
@@ -29,6 +33,8 @@ class MemoryStructure:
         payload: bytes
         link: object = None
 
+        # less than and equals are minimum definitions needed for
+        # total ordering
         def __lt__(self, right):
             return self.addr < right.addr
 
@@ -36,12 +42,19 @@ class MemoryStructure:
             return self.addr == right.addr
 
         def __iter__(self):
+            """
+            Iterate over the payload writes as if it were a linked list
+            """
             this = self
             while this is not None:
                 yield this
                 this = this.link
 
         def chain(self, other):
+            """
+            Apppends one payload to another for chained writes. Payloads combined in this fashion act as linked lists.
+            Ordering in the list is determined by payload order (by address)
+            """
             pchain = [*self]
             # NOTE: assumes we're keeping things in sorted order
             # from the beginning
@@ -55,7 +68,11 @@ class MemoryStructure:
 
         def __rshift__(self, bindata):
             """
-            # rom = struct @ b"\xff\xff" >> rom
+            Syntactic sugar to have the payload data be inserted into a byte stream.
+            i.e.
+            rom = struct @ b"\xff\xff" >> rom
+
+            Note that this will do *all* writes starting from this link in the chain.
             """
             bindata = bytearray(bindata)
             for p in [*self]:
@@ -64,9 +81,16 @@ class MemoryStructure:
             return bytes(bindata)
 
     def as_tuple(self):
+        """
+        Get the beginning and end addresses of the structure.
+        """
         return (self.addr, self.addr + self.length)
 
     def get_pointer_write(self, dst_addr, ptr_len):
+        """
+        Generates a new MemoryStructure, pointing to `dst_addr` which will write the address of this structure to that location.
+        `ptr_len` is required to determine the size of the pointer to be written.
+        """
         return MemoryStructure(dst_addr, ptr_len,
                                name=f"ptr_overwrite_{dst_addr}",
                                descr=f"Overwriting pointer to destination "
@@ -74,6 +98,11 @@ class MemoryStructure:
                self.addr.to_bytes(2, byteorder="little")
 
     def split(self, size, start=None):
+        """
+        Split a Memory structure into two, one starting at `start` with size `size`, and the remainder starting thereafter and running to the end of the block.
+        NOTE: If `start` is not specified, then it defaults to the parent block address. If a `start` other than this address is provided, the lower part of the block not addressed will be "lost" in the split.
+        The caller is responsible for tracking the "missing" stretch.
+        """
         start = start or self.addr
         return (
             MemoryStructure(addr=start, length=size,
@@ -86,6 +115,9 @@ class MemoryStructure:
         )
 
     def subdivide(self, size):
+        """
+        Split this block into equally sized blocks with length `size`. The remainder after equal subdivision, if any, is returned as well.
+        """
         nsplit = math.ceil(self.length / size)
         blocks = list(self.split(size))
         for i in range(2, nsplit):
@@ -101,8 +133,11 @@ class MemoryStructure:
             blocks.append(blk2)
         return blocks
 
-    # FIXME: reconcile with use portion interval library
     def compare(self, other):
+        """
+        Basic interval comparison. Checks if two blocks are joinable, e.g., adjacent.
+        FIXME: reconcile with use portion interval library
+        """
         lower, upper = self, other
         if self.addr > other.addr:
             lower, upper = upper, lower
@@ -119,9 +154,16 @@ class MemoryStructure:
         return -1
 
     def __lt__(self, other):
+        """
+        Convenience operator to compare blocks.
+        Note that equality is not handled, because of interval math.
+        """
         return self.addr < other.addr
 
     def __add__(self, other):
+        """
+        If this and `other` are joinable (compare returns 1), then create a new block as their union.
+        """
         if self.compare(other) != 1:
             raise ValueError(f"{self} + {other} are not disjoint and adjacent.")
         lower, upper = min(self, other), max(self, other)
@@ -133,9 +175,15 @@ class MemoryStructure:
         )
 
     def __matmul__(self, bindata):
+        """
+        Syntactic sugar for `patch`.
+        """
         return self.patch(bindata)
 
     def patch(self, data, bindata=None):
+        """
+        Create and deploy the `Payload` object. If `bindata` is provided, deploy immediately, else return the payload.
+        """
         assert len(data) == self.length, f"0x{self.addr:x}+{self.length}"
         if bindata is None:
             return self.Payload(addr=self.addr, payload=data)
@@ -152,12 +200,18 @@ class MemoryStructure:
         return bytes(bindata[self.addr:self.addr+self.length])
 
     def deserialize(self, bindata):
+        """
+        Return a dictionary representation of this object with some metadata. Designed for use with JSON serialization.
+        """
         return {**asdict(self),
                 "_type": self.__class__.__name__,
                 "_data": self.read(bindata)}
 
     @classmethod
     def chain_write(cls, writes):
+        """
+        Convenience method to generate several chained writes from a list.
+        """
         payloads = []
         for addr, data in writes.items():
             blk = cls(addr=addr, length=len(data),
@@ -172,23 +226,31 @@ class MemoryStructure:
 
     @classmethod
     def find_free_space(cls, bindata, min_length=16, empty_byte=0xFF):
-        addr = 0
-        blocks = []
+        """
+        Looks for stretches of `min_length` or greater repetitions of `empty_byte` in `bindata`.
+        Returns blocks describing these stretches.
+        """
+        addr, blocks = 0, []
         while addr < len(bindata):
+            # Find the next position of an empty_byte, starting one
+            # beyond the current address. If none is found, we're done.
             try:
                 addr = st_addr = bindata.index(empty_byte, addr)
                 addr += 1
             except ValueError:
                 break
+            # Count up the length of this stretch
             while addr < len(bindata) and bindata[addr] == empty_byte:
                 addr += 1
 
+            # If it meets the condition, define a new block
             blklen = addr - st_addr
             if blklen >= min_length:
                 i = len(blocks)
                 blocks.append(cls(addr=st_addr, length=blklen, name=f"free_space_{i}",
                                   descr=f"Free space: {blklen} bytes"))
 
+            # move pointer to next byte for next search
             addr += 1
 
         return blocks
@@ -196,13 +258,20 @@ class MemoryStructure:
 
     @classmethod
     def serialize(cls, json_repr):
+        """
+        Helper method for serialization purposes.
+        In coordination with `deserialize`, pops the "raw data" out of the JSON object and attempts to recreate the object.
+        """
         _data = json_repr.pop("_data", None)
         assert isinstance(_data, bytes)
         return _data
 
     @classmethod
     def from_json(cls, json_repr):
-        # TODO: use type information
+        """
+        Generate a new MemoryStructure from a JSON representation. See `serialize` and `deserialize`.
+        TODO: use type information
+        """
         _type = json_repr.pop("_type", None)
         _data = json_repr.pop("_data", None)
         return cls(**json_repr)
@@ -213,6 +282,10 @@ class MemoryStructure:
 
 # TODO: should be datatable
 class SNESHeader(MemoryStructure):
+    """
+    Structure representing a standard SNES header. It is used to identify the cartridge by Nintendo and manufacturers.
+    NOTE: this is not the 0x200 byte "header" attached to the rom image, but an internal memory block at a specific address in all SNES images, holding metadata in a specific format.
+    """
     def __init__(self):
         self._unpack_schema = self._generate_schema()
 
@@ -232,6 +305,10 @@ class SNESHeader(MemoryStructure):
         return decode_tbl
 
 class Registry:
+    """
+    The `Registry` object is a container for MemoryStructures and related constructs.
+    It also has a tagging mechanism to be able to retrieve related stuctures easily.
+    """
     @classmethod
     def copy(cls, reg):
         cpy = cls()
@@ -247,6 +324,9 @@ class Registry:
         self._tags = defaultdict(set)
 
     def register_block(self, addr, length, name, descr, tags=set()):
+        """
+        Register a new `MemoryStructure` from metadata, potentially with `tags`.
+        """
         if name in self._blocks:
             return self._blocks[name]
 
@@ -259,6 +339,9 @@ class Registry:
         return block
 
     def deregister_block(self, block):
+        """
+        Removes the block from the registry and its associated metadata, returning it.
+        """
         self._tree.pop(block.as_tuple())
         for tag, blk_list in self._tags.items():
             blk_list.remove(block.name)
@@ -269,10 +352,16 @@ class Registry:
         return pprint.pformat(self._blocks)
 
     def find_blks_from_addr(self, addr):
+        """
+        From a given address, find all blocks that contain this address and return them.
+        """
         return {block.name: block for span, block in self._tree.items()
                                   if addr >= span[0] and addr < span[1]}
 
     def format_tags(self, tag, sort_by=None):
+        """
+        String formatting helper for tags, with optional `sort_by == {addr, name}`.
+        """
         frmt = {name: self._blocks[name].addr for name in self._tags[tag]}
         frmt = frmt.items()
         if sort_by == "addr":
