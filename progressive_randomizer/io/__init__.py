2b0d438663bf3e399dbc7d1757e3ecf55c501f1f
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-09-06 20:22:08 -0700

    io: some fixes and enhancements

--- progressive_randomizer/io/__init__.py
+++ progressive_randomizer/io/__init__.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import asdict
+from ..utils import ByteUtils
 
 class DataclassJSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -20,6 +21,20 @@ class BaseEmuIO:
 
     def read_memory(self, st, en):
         pass
+
+    def read_memory_as(self, st, en=None, width=None, unpack=True):
+        data = self.read_memory(st, en or st + 1)
+        if len(data) == 1 and unpack:
+            return data[0]
+
+        if width == 1:
+            return ByteUtils.as_uint_8(data, unpack)
+        elif width == 2:
+            return ByteUtils.as_uint_16(data, unpack)
+        elif width is not None:
+            return ByteUtils.as_uint_arb(data, width, unpack)
+
+        return data
 
     def write_memory(self, st, val):
         pass
