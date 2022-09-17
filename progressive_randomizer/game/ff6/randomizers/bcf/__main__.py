630a7aa171dbc0d6b1cf3128ac825f5b034fa0a0
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-09-17 08:51:27 -0700

    bcf: simplify main initialization sequence

--- progressive_randomizer/game/ff6/randomizers/bcf/__main__.py
+++ progressive_randomizer/game/ff6/randomizers/bcf/__main__.py
@@ -3,21 +3,7 @@ import logging
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger()
 
-import time
-from .observer import BCFObserver
+from .bot import BCF
 
 if __name__ == "__main__":
-    #bot._last_state_drop = -1
-    #bot.run()
-
-    obs = BCFObserver()
-    obs.register_user("TEST USER")
-    obs._users["TEST USER"] = {"score": 0, "char": "Terra", "area": "WoB Overworld"}
-    while True:
-        obs.process_change()
-        #log.info(str(time.time()))
-        #log.info(obs._context)
-        log.info(obs._game_state.play_state.name)
-        log.info(obs._context)
-        log.info(obs.context)
-        time.sleep(1)
+    bot.run()
